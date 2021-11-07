ba758808fe2233585d7e6818b56cfa7044c1067c
LouisAXI <louis.xia18@gmail.com>
2021-11-07 18:41:33 +0100

    2eme Commit

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 
 class Summary extends Component {
     render() {
+        const numberCommit = this.props
+            .data
+            .viewer
+            ?.repositories
+            .nodes
+            .reduce((previous, current) => previous + current.ref === undefined ? 0 : current.ref.target.history.totalCount)
+
+        const numberLine = ()=>{
+
+        }
         return (
-            <div>
-                {this.props.data.viewer?.login}
-            </div>
+            <>
+                <img src={this.props.data.viewer?.avatarUrl} alt="Avatar" />
+                <div>{numberCommit}</div>
+                <div>
+                    followers: {this.props.data.viewer?.followers.totalCount}
+                </div>
+                <div>
+                    following: {this.props.data.viewer?.following.totalCount}
+                </div>
+            </>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import Summary from './components/Summary';
 import Overview from './components/Overview';
 import Language from './components/Language';
 import Repositories from './components/Repositories';
+import './styles/styles.scss'
 
 const httpLink = new HttpLink({
   uri: 'https://api.github.com/graphql',
@@ -23,7 +24,7 @@ const authMiddleware = new ApolloLink(
   (operation, forward) => {
       operation.setContext({
           headers: {
-              authorization: 'bearer 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',
+              authorization: 'bearer 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',
           },
       });
 
@@ -40,48 +41,81 @@ const client = new ApolloClient({
 
 
 const GITHUB_DATA_QUERY = gql`
-query { 
-  viewer { 
-    login
-  }
+query {
+    viewer {
+        login
+        repositories(first: 6) {
+            totalCount
+            pageInfo {
+                hasNextPage
+                hasPreviousPage
+                startCursor
+                endCursor
+            }
+            nodes {
+                name
+                ref(qualifiedName: "main") {
+                    target {
+                    ... on Commit {
+                            additions
+                            deletions
+                            history {
+                                totalCount
+                                edges {
+                                    node {
+                                        committedDate
+                                    }
+                                }
+                            }
+                        } 
+                    }
+                }
+                languages(first:100) {
+                    nodes {
+                        name
+                    }
+                }
+            }
+        }
+        following {
+            totalCount
+        }
+        followers {
+            totalCount
+        }
+        avatarUrl
+        bio
+    }
 }
 `;
 
 function GitData() {
+    const { loading, error, data } = useQuery(GITHUB_DATA_QUERY);
 
-  const { loading, error, data } = useQuery(GITHUB_DATA_QUERY);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data;
+    return data;
 }
 
 
 function App() {
-  const data = GitData();
-  return (
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <div>{data.viewer?.login}</div>
-      <Summary data={data}/>
-      <Overview data={data}/>
-      <Language data={data}/>
-      <Repositories data={data}/>
-    </div>
-  );
+    const data = GitData();
+    return (
+        <div className="container-all">
+            <Summary data={data}/>
+            <Overview data={data}/>
+            <Language data={data}/>
+            <Repositories data={data}/>
+        </div>
+    );
 }
 
 render(
-
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root'),
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'),
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
